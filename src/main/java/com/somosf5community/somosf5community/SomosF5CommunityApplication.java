156d2f187f0538f75c0fb0d16df52d613d4501a5
Enol-Igareta <enol1code@gmail.com>
2023-03-20 14:43:40 +0100

    init repo

---
+++ src/main/java/com/somosf5community/somosf5community/SomosF5CommunityApplication.java
@@ -0,0 +1,13 @@
+package com.somosf5community.somosf5community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomosF5CommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomosF5CommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somosf5community/somosf5community/SomosF5CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somosf5community.somosf5community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomosF5CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
