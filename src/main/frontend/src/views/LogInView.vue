8933298e0bd69e55b24401e1c1436a0a369682e4
GabriMF <108740750+GabriMF@users.noreply.github.com>
2023-03-23 12:15:52 +0100

    update branch

--- src/main/frontend/src/views/LogInView.vue
+++ src/main/frontend/src/views/LogInView.vue
@@ -1,14 +1,16 @@
 <script>
-
+    import HeaderSigIn from '../components/HeaderSigIn.vue'
 </script>
 <template>
+    <HeaderSigIn/>
     <div id="logInPage">
         <h2>login</h2>
     </div>
 </template>
 <style scoped lang="scss">
     #logInPage{
-        background-color: orange;
-        
+        background-color: #FF4700;
+        width: 100vw;
+        height: 100vh;
     }
 </style>
