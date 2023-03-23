1fe7b177e9c46333fb2e363a91b022c80dab7a99
GabriMF <108740750+GabriMF@users.noreply.github.com>
2023-03-23 15:27:59 +0100

    basic form styles done

--- src/main/frontend/src/components/LogInComp.vue
+++ src/main/frontend/src/components/LogInComp.vue
@@ -1,56 +1,81 @@
 <script>
+export default {
+    data: () => ({
+        valid: true,
+        mail: '',
+        mailRules: [
+            v => !!v || 'Name is required',
+            v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+        ],
+        select: null,
 
+        valid: true,
+        password: '',
+        passwordRules: [
+            v => !!v || 'Password is required',
+            v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+        ],
+        select: null,
+        // items: [
+        //     'Item 1',
+        //     'Item 2',
+        //     'Item 3',
+        //     'Item 4',
+        // ],
+        // checkbox: false,
+    }),
+
+    methods: {
+        async validate() {
+            const { valid } = await this.$refs.form.validate()
+
+            if (valid) alert('Form is valid')
+        },
+        reset() {
+            this.$refs.form.reset()
+        },
+        resetValidation() {
+            this.$refs.form.resetValidation()
+        },
+    },
+}
 </script>
+
 <template>
-    <div id="logIn">
-        <h1 id="headerForm">Bienvenido a SomosF5</h1>
-        <form id="logInForm" action="">
-            <p>
-                <label id="mailLabel">Correo Electronico</label>
-                <input id="mailInput" v-model="mail" type="text" name="mail">
-            </p>
-            <p>
-                <label id="passwordLabel">Contraseña</label>
-                <input id="passwordInput" v-model="password" type="password" name="password">
-            </p>
-            <p>
-                <input id="submitButton" type="submit" value="Enviar">
-            </p>
-        </form>
+    <div class="logIn">
+        <h1 class="headerForm">Bienvenido a SomosF5</h1>
+        <v-sheet class="mx-auto">
+            <v-form ref="form">
+                <v-text-field v-model="mail" :counter="10" :rules="mailRules" label="Correo Electronico" required></v-text-field>
+                <v-text-field v-model="password" :counter="10" :rules="nameRules" label="Contraseña" required></v-text-field>
+                <div class="d-flex flex-column">
+                    <v-btn color="success" class="mt-4" block @click="validate">
+                        Validate
+                    </v-btn>
+                </div>
+            </v-form>
+        </v-sheet>
     </div>
 </template>
 <style lang="scss">
-    #logIn{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        #headerForm{
-            font-size: 10vh;
-            color: white;
-        }
-        #logInForm{
-            #mailLabel{
-                display: flex;
-                justify-content: center;
-            }
-            #mailInput{
-                background-color: white;
-            }
-            #passwordLabel{
-                display: flex;
-                justify-content:center;
-            }
-            #passwordInput{
-                background-color: white;
-            }
-            #submitButton{
-                display: flex;
-                justify-content: center;
-            }
-        }
+.logIn {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
+    .headerForm {
+        font-size: 10vh;
+        color: white;
     }
-
-
+    .v-sheet{
+        width: 40vw;
+        .v-form{
+            background-color: #FF4700;
+            .v-input__control{
+                background-color: white;
+            }
+        }
+    }
+}
 </style>
 
