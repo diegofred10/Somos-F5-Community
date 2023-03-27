d6f0740220823346d3421461a8f716cb77b68660
Enol-Igareta <enol1code@gmail.com>
2023-03-27 14:09:40 +0200

    o2o user progile

--- src/main/java/com/somosf5community/models/User.java
+++ src/main/java/com/somosf5community/models/User.java
@@ -2,6 +2,7 @@ package com.somosf5community.models;
 
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -10,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,6 +27,10 @@ public class User {
 
     private String username;
     private String password;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "profile_id")
+    private Profile profile;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,7 @@
--- INSERT INTO profiles (id_profile, github, linkedin, location, name, surname) VALUES (default, "github", "linkedin", "Gijon", "Pedro", "picapiedras")
--- INSERT INTO user_roles (user_id, role_id) VALUES (1,1)
--- INSERT INTO users (id_user, email, password, profile_id) VALUES (default, "hola@jaja.xd", "1234", 1)
+
 INSERT INTO users (id, username, password) VALUES (default, "enol", "1234")
 INSERT INTO roles (id, role_name) VALUES (default, "ROLE_ADMIN")
 INSERT INTO roles (id, role_name) VALUES (default, "ROLE_USER")
 INSERT INTO user_roles (user_id, role_id) VALUES (1,1)
 
--- INSERT INTO users_roles (id_user,id_role) VALUES (1, 1)
 
