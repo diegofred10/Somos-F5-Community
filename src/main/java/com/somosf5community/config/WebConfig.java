615e3a831506a725a0e12078925c906e96ba6f05
SaulAguinaga <saul_aguinaga@hotmail.es>
2023-03-28 13:55:41 +0200

    merge develop

--- src/main/java/com/somosf5community/config/WebConfig.java
+++ src/main/java/com/somosf5community/config/WebConfig.java
@@ -4,17 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-<<<<<<< HEAD
-@Configuration
-public class WebConfig implements WebMvcConfigurer{
-    
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-            .addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET","POST","PUT","DELETE","OPTION");
-=======
 
 @Configuration
 public class WebConfig implements  WebMvcConfigurer {
@@ -26,6 +15,5 @@ public class WebConfig implements  WebMvcConfigurer {
         .allowedOrigins("http://localhost:9000")
         .allowCredentials(true)
         .allowedMethods("GET", "POST", "DELETE", "OPTION", "PUT");
->>>>>>> develop
     }
 }
