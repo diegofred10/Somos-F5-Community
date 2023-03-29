6f8b4cece9a0dcdbcbbcc0e61e7fa460820938a3
SaulAguinaga <saul_aguinaga@hotmail.es>
2023-03-29 10:57:05 +0200

    crud basic

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,7 @@
 INSERT INTO profiles VALUES (default, 'https://github.com/SaulAguinaga', 'https://www.linkedin.com/in/saulaguinagaestevez/', 'Gijón', 'Saul', 'Aguinaga')
 INSERT INTO users VALUES (default, 'saul@gmail.com','password', 1)
 INSERT INTO posts VALUES (default, '2023-02-01','Recurso de prueba', 'Titulo de prueba', 1)
-INSERT INTO users (id, username, password) VALUES (default, "enol", "1234")
+INSERT INTO users (id, username, password) VALUES (default, "enol", "1234",2)
 INSERT INTO roles (id, role_name) VALUES (default, "ROLE_ADMIN")
 INSERT INTO roles (id, role_name) VALUES (default, "ROLE_USER")
 INSERT INTO user_roles (user_id, role_id) VALUES (1,1)
