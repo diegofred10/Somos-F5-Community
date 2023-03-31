014e4520da4c1fdfe8401fc41ebe7e6f8ba4b7c0
Enol-Igareta <enol1code@gmail.com>
2023-03-31 20:40:30 +0200

    subiendo imagenes

--- src/main/java/com/somosf5community/services/StorageService.java
+++ src/main/java/com/somosf5community/services/StorageService.java
@@ -1,11 +1,14 @@
 package com.somosf5community.services;
 
+import java.io.IOException;
+
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface StorageService {
-    void init();
+    void init() throws IOException;
 
     String store(MultipartFile file);
 
-    Resource loadAsResource(String filename)
+    Resource loadAsResource(String filename);
 }
