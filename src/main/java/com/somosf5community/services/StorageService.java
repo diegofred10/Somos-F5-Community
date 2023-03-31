aa5f38576a8500f6fbd2cf112c29a525cda2e306
Enol-Igareta <enol1code@gmail.com>
2023-03-31 14:25:23 +0200

    rama imagen

---
+++ src/main/java/com/somosf5community/services/StorageService.java
@@ -0,0 +1,11 @@
+package com.somosf5community.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+    void init();
+
+    String store(MultipartFile file);
+
+    Resource loadAsResource(String filename)
+}
