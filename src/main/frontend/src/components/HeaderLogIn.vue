8653c3610afb7a3e7f2fd595b9f08fb1c9d66758
alcfdez <alcfdez@hotmail.com>
2023-03-31 14:06:22 +0200

    header logIn fix

--- src/main/frontend/src/components/HeaderLogIn.vue
+++ src/main/frontend/src/components/HeaderLogIn.vue
@@ -1,20 +1,34 @@
-<script>
-
-</script>
+<script></script>
 <template>
-    <div id="header">
-        <img id="headerLogo" src="../assets/images/imagesSomosF5/Logo.png" alt="Somos F5">
-    </div>
+  <div class="header">
+    <a href="/" aria-current="page" class="headerLogo"></a>
+  </div>
 </template>
 <style lang="scss">
-    #header{
-        background-color: #FED2C0;
-        width: 100vw;
-        height: 15vh;
-        #headerLogo{
-            width: 15vw;
-            height: 10vh;
-        }
-    }
-</style>
+@use "@/scss/colors" as c;
+@use "@/scss/mixins" as m;
 
+.header {
+  position: fixed;
+  left: 0%;
+  top: 0%;
+  right: 0%;
+  bottom: 0%;
+  display: flex;
+  height: 5rem;
+  -webkit-box-pack: center;
+  justify-content: center;
+  background-color: map-get(c.$colors, "light-orange");
+
+  .headerLogo {
+    background-image: url("https://uploads-ssl.webflow.com/62e2b7b9c42bdda27c83d493/6329c1bc9e1976ae0ada64b0_somos-f5.svg");
+    background-size: contain;
+    background-position: 0% 50%;
+    background-repeat: no-repeat;
+    width: 9rem;
+    height: 4rem;
+    margin: auto;
+    margin-left: 1vw;
+  }
+}
+</style>
