23229305b4fa78fe0ea5baa542212068cb72ea39
SaulAguinaga <saul_aguinaga@hotmail.es>
2023-04-03 10:14:24 +0200

    pull develop

--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -1,20 +1,8 @@
 <script setup>
-<<<<<<< HEAD
-import TheWelcome from '../components/TheWelcome.vue'
-import UploadFiles from '../components/UploadFiles.vue';
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-    <UploadFiles></UploadFiles>
-  </main>
-=======
 import FeedbackView from './FeedbackView.vue';
 </script>
 
 <template>
 <FeedbackView/>
->>>>>>> f331e1f0a08f249a9b740bef23d800b29853f2d7
 </template>
 
