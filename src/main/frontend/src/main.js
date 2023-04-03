aebfb77a21eff51d82fa2c0efa36d526727feea8
SaulAguinaga <saul_aguinaga@hotmail.es>
2023-04-03 09:42:28 +0200

    pull develop

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from "axios";
 
 import App from './App.vue'
 import router from './router'
 
 import './scss/main.scss'
+
+axios.defaults.baseURL = 'https://localhost:8080/api'
 
 const app = createApp(App)
 
--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import UploadFiles from '../components/UploadFiles.vue';
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <UploadFiles></UploadFiles>
   </main>
 </template>
