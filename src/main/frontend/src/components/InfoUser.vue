cb94bf57a0590cceeefe2970f0e59759d01f64ca
SaulAguinaga <saul_aguinaga@hotmail.es>
2023-04-05 12:52:03 +0200

    inputs changes

--- src/main/frontend/src/components/InfoUser.vue
+++ src/main/frontend/src/components/InfoUser.vue
@@ -64,37 +64,50 @@ const store = useAuthStore();
 </script>
 
 <template>
-<div class="infoUser">
-    <div class="photoUser">
-        <input type="file" @change="onFileChange" ref="fileInput" style="display:none">  
-        <button @click="$refs.fileInput.click()">    
-        <img class="imgProfile" :src="'http://localhost:8080/media/' + user.image" alt="img">
-    </button>
-    </div>
-    <div class="contacts">
-        <input v-if="profile.name==null" placeholder="Nombre" class="name"> 
-        <input v-else :placeholder=profile.name  class="name">
-        <div class="contact">
-            <img class="logo" src="../assets/images/imagesSomosF5/github.png" alt="gitHub">
-            <p class="contactsName">{{ profile.github }}</p>
+<div class="banner">
+    <div class="infoUser">
+        <div class="photoAndContact">
+            <div class="photoUser">
+                <input type="file" @change="onFileChange" ref="fileInput" style="display:none">  
+                <button @click="$refs.fileInput.click()">     
+                <img class="imgProfile" v-if="user.image!=null" :src="'http://localhost:8080/media/' + user.image" alt="img">
+                <img class="imgProfile" v-else src="../assets/images/perfilVacio.png" alt="img">
+                </button>
+            </div>
+            <div class="contacts">
+                <div class="contact">
+                    
+                    <input v-if="profile.name==null" placeholder="Nombre" class="name"> 
+                    <input v-else :placeholder=profile.name  class="name">
+                    <i @click="" class="fa-solid fa-pen"></i>
+                <div class="contact">
+                    <img class="logo" src="../assets/images/imagesSomosF5/github.png" alt="gitHub">
+                    <input v-if="profile.github==null" placeholder="GitHub" class="contactsName"> 
+                    <input v-else :placeholder=profile.github  class="contactsName">
+                </div>
+                <div class="contact">
+                    <img class="logo" src="../assets/images/imagesSomosF5/linkedin.png" alt="linkedin">
+                    <input v-if="profile.linkedin==null" placeholder="LinkedIn" class="contactsName"> 
+                    <input v-else :placeholder=profile.linkedin class="contactsName">
+                </div>
+                <div class="contact">
+                    <img class="logo" src="../assets/images/imagesSomosF5/Vector.png" alt="email">
+                    <input v-if="profile.email==null" placeholder="Email" class="contactsName"> 
+                    <input v-else :placeholder=profile.email class="contactsName">
+                </div>
+                <div class="contact">
+                    <img class="logo" src="../assets/images/imagesSomosF5/geo-alt.png" alt="geo">
+                    <input v-if="profile.location==null" placeholder="Ubicación" class="contactsName"> 
+                    <input v-else :placeholder=profile.location class="contactsName">
+                </div>
+            </div>   
+        </div> 
         </div>
-        <div class="contact">
-            <img class="logo" src="../assets/images/imagesSomosF5/linkedin.png" alt="linkedin">
-            <p class="contactsName">{{ profile.linkedin }}</p>
+        <div class="design">       
+            <img class="arrow" src="https://uploads-ssl.webflow.com/62e2b7b9c42bdda27c83d493/632a9e30dd20f538a3049cc4_shape-13.svg" alt="">
+            <img class="triangle" src="https://uploads-ssl.webflow.com/62e2b7b9c42bdda27c83d493/6329c1bbaf7322b42359abe7_shape-5.svg" alt="">       
+            <img class="semicircle" src="https://uploads-ssl.webflow.com/62e2b7b9c42bdda27c83d493/63c7aad88f903d7f30ff4eff_emp-shape-05.svg" alt="">
         </div>
-        <div class="contact">
-            <img class="logo" src="../assets/images/imagesSomosF5/Vector.png" alt="email">
-            <p class="contactsName">{{ profile.email }}</p>
-        </div>
-        <div class="contact">
-            <img class="logo" src="../assets/images/imagesSomosF5/geo-alt.png" alt="geo">
-            <p class="contactsName">{{ profile.location }}</p>
-        </div>   
-    </div>
-    <div class="design">       
-        <img class="arrow" src="https://uploads-ssl.webflow.com/62e2b7b9c42bdda27c83d493/632a9e30dd20f538a3049cc4_shape-13.svg" alt="">
-        <img class="triangle" src="https://uploads-ssl.webflow.com/62e2b7b9c42bdda27c83d493/6329c1bbaf7322b42359abe7_shape-5.svg" alt="">       
-        <img class="semicircle" src="https://uploads-ssl.webflow.com/62e2b7b9c42bdda27c83d493/63c7aad88f903d7f30ff4eff_emp-shape-05.svg" alt="">
     </div>
 </div>
 </template>
@@ -106,68 +119,59 @@ const store = useAuthStore();
 .banner{
     display: flex;
     justify-content: center;
+    width: 100%;
 
-.infoUser{
-background-color: map-get(c.$colors,"orange");
-height:auto;
-width: 90%;
-display: flex;
-align-items: center;
-justify-content: space-around;
-margin: 2%;
-
-  .photoAndContact{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 25vw;
-
-    .photoUser{
-        width: 45%;
-
-        .imgProfile{
-            border-radius: 100%;
-        }
-    }
-
-    .contacts{
+    .infoUser{
         display: flex;
-        flex-direction: column;
-        
-        .name{
-            font-size: 2em;
-            color: map-get(c.$colors,"white");
-            font-family: 'openSans';
-            font-weight: bold;
-            
-        }
-        .contact{
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            font-family: 'openSans' ;
-            margin-bottom: 0.5em;
+        width: 90%;
+        background-color: map-get(c.$colors,"orange");
 
-            .logo{
-                width: 10%;
+    .photoAndContact{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 60%;
+            .photoUser{
+                width: 50%;
 
-
+                .imgProfile{
+                    border-radius: 100%;
+                }
             }
-            .contactsName{
-                color: white;
-                margin-left: 2%;
+            .contacts{
+                display: flex;
+                flex-direction: column;
+                
+                .name{
+                    font-size: 2em;
+                    color: map-get(c.$colors,"white");
+                    font-family: 'openSans';
+                    font-weight: bold;
+                    
+                }
+                .contact{
+                    display: flex;
+                    flex-direction: column;
+                    font-family: 'openSans' ;
+                    margin-bottom: 0.5em;
+                    input{
+                        width: 50%;
+                    }
+                    .logo{
+                        width: 5%;
+                    }
+                    .contactsName{
+                        color: white;
+                        margin-left: 2%;
+                    }
+                }
             }
         }
-
-    }
-}
     .design{
         display: flex;
-        justify-content: flex-end;
+        // justify-content: flex-end;
         flex-direction: row;
-        width: 40%;
-  
-        
+        width: 35%;
         .arrow{
             width: 30%;          
             z-index: 1;  
@@ -184,8 +188,7 @@ margin: 2%;
             width: 40%;
         }
     }
-}
-
+    }
 }
 
 </style>
