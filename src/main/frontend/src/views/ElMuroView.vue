523819ea9fa84d30295e0c68000ddb6120a78367
alcfdez <alcfdez@hotmail.com>
2023-04-20 14:23:29 +0200

    search

--- src/main/frontend/src/views/ElMuroView.vue
+++ src/main/frontend/src/views/ElMuroView.vue
@@ -1,24 +1,43 @@
 <script setup>
-import Header from '../components/Header.vue';
-import UserFeed from '../components/UserFeed.vue';
-import AddPublication from '../components/AddPublication.vue';
-import Search from '../components/Search.vue'
-import PostComp from '../components/PostComponent.vue'
+import Header from "../components/Header.vue";
+import UserFeed from "../components/UserFeed.vue";
+import CardProfile from "../components/CardProfile.vue";
+import PostService from "../services/PostService";
+import { ref, onBeforeMount } from "vue";
 
+const postService = new PostService();
+
+let posts = ref([]);
+
+onBeforeMount(async () => {
+  await postService.fetchAllPost();
+  posts.value = postService.getPost();
+  console.log(posts.value);
+});
 </script>
 
 <template>
-<main>
-<Header/>
-<UserFeed/>
-<div class="tools">
-    <!-- <AddPublication/>    -->
-    <Search/>
-</div>
-<div class="publi">
-    <PostComp/>
-</div>
-</main>
+  <main>
+    <Header />
+    <UserFeed />
+
+    <div class="searcher">
+    <input class="text" type="text" v-model="input" placeholder= "  buscar.." />
+    <div class="itemPosts" v-for="post in filterList()" :post="post">
+      <p>{{ post }}</p>
+    </div>
+
+    <div class="itemError" v-if="input && !filter().length">
+      <p>No hay resultados</p>
+    </div>
+  </div>
+    <div class="publi">
+
+      <CardProfile v-for="post in posts" :post="post" />
+
+    
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
@@ -32,32 +51,53 @@ main {
   align-items: center;
   flex-direction: column;
 
+  .searcher {
+  background-color: map-get(c.$colors, "light-purple");
+  margin-top: 1vw;
+  width: 60vw;
+  height: 7vh;
+  border-radius: 3px;
+  .text {
+    width: 60vw;
+    font-family: "Open Sans", sans-serif;
+    font-size: 2vw;
 
-.tools {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 90vw;
+    ::placeholder
+    :root{
+      --fa-style-family: "Font Awesome 6 Free";
+      --fa-style: 200; 
+      --fa-border-color: WhiteSmoke;
+    --fa-border-padding: 2em;
+    }
+
+  }
+
+  .itemError {
+    width: 60vw;
+    font-family: "Open Sans", sans-serif;
+    font-size: 2vw;
+  }
+}
 
     .modal-container {
-        align-self: start;
+      align-self: start;
 
-        .modal {
-            background: map-get(c.$colors, "white");
+      .modal {
+        background: map-get(c.$colors, "white");
 
-            display: flex;
-            border: 2px solid black;
-            width: 100%;
-            // padding: 2em;
-            font-size: 1.2em;
-            color: black;
-            height: 2em;
+        display: flex;
+        border: 2px solid black;
+        width: 100%;
+        // padding: 2em;
+        font-size: 1.2em;
+        color: black;
+        height: 2em;
 
-            .btn-add {
-                margin-left: 1em;
-            }
+        .btn-add {
+          margin-left: 1em;
         }
+      }
     }
-}
-}
+  }
+
 </style>
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,17 @@
 
 INSERT INTO profiles VALUES (1, 'MIMIMI', 'LinkedIn', 'Ubicacion', 'Nombre', 'Apellido')
 -- INSERT INTO profiles VALUES (default, 'Giiiii', 'Liiiii', 'Ubiiiii', 'Nomiiiii', 'Apiiiiii')
+
 INSERT INTO users VALUES (default, null, '$2a$10$GesR/t2ldE7wtXwh93Brd.txPEvLVbtE02LOwWmI9Rq40YWKWi4Ra', 'saul@gmail.com', 1)
--- INSERT INTO posts VALUES (default, '2023-02-01','Recurso de prueba', 'Titulo de prueba', 1)
 -- INSERT INTO users (id, username, password, profile_id) VALUES (default, "enol", "1234", 1)
+
 INSERT INTO roles (id, role_name) VALUES (default, "ROLE_ADMIN")
 INSERT INTO roles (id, role_name) VALUES (default, "ROLE_USER")
+
 INSERT INTO user_roles (user_id, role_id) VALUES (1,1)
 
 
+-- INSERT INTO posts VALUES (default, '2023-02-01','Recurso de prueba', 'Titulo de prueba', 1)
+INSERT INTO posts (id_post, title, date, description, image, profile_id) VALUES (default, 'publicacion', null, 'aplicacion java', 'null', 1)
+INSERT INTO posts (id_post, title, date, description, image, profile_id) VALUES (default, 'VUE', null, 'aplicacion VUETIFY', 'null', 1)
+INSERT INTO posts (id_post, title, date, description, image, profile_id) VALUES (default, 'mysql', null, 'aplicacion workbench', 'null', 1)
